edbf17c1bb1afae56b00e581c9d51c65abf18a7c
Federico Giaj <44998394+freddy-jay@users.noreply.github.com>
2021-02-10 11:09:22 +0100

    Delete src directory

--- src/tetris/logic/Point.scala
+++
@@ -1,8 +0,0 @@
-package tetris.logic
-
-// you can alter this file!
-
-case class Point(x : Int, y : Int) {
-  def +(rhs : Point) : Point = Point(x + rhs.x, y + rhs.y)
-  def ==(rhs : Point) : Boolean = x == rhs.x && y == rhs.y
-}
